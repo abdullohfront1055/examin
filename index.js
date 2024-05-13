1530aa4732478423270de33b9e9f0dbbba052bfb
Abdulloh Xo'jayev <xodjaevabdulloh54@gmail.com>
2024-05-13 10:26:08 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,47 @@
+// 1-savol Arifmetik savollar
+// alert(5 % 2);
+
+// 2- savol Math metodlar
+// let son = Math.floor(Math.random() * 10)
+// alert(son)
+
+// 3-savol Math metodlar
+// let son = 12.510
+// let butunson = Math.floor(son)
+// alert(butunson)
+
+// 4-savol function
+// let soz = ("Mars It school");
+// function uzunlik() {
+//     alert(soz.length);
+// }uzunlik();
+
+// 5-savol for loop
+// let soz = ("Mars IT school");
+// for( let i = 0; i < 10; i++){
+//     console.log(soz)
+// }
+
+// 6-savol Array Methods
+// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+// let ism = harflar[0] + harflar[1] + harflar[2] + harflar[20] + harflar[11] + harflar[11] + harflar[14] + harflar[7]
+
+//  7-savol if else
+//   let yosh = prompt("Yosh kiriting");
+//   if (yosh > 18){
+//     console.log("Ballogat yoshiga yetkansiz")
+//   }else{
+//     console.log("Ballogat yoshiga yetmagansiz")
+//   }
+ 
+//  8-savol String metodlar
+//  let ism = prompt("Ismingizni kiriting");
+//  let harflar = ism.split('');
+//  let teskari = harflar.reverse().join('');
+//  alert(teskari);
+
+// 10-savol Array methodlar
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let natija = [];
+// console.log(sonlar);
+
